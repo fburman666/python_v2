23ea14ba115889781a8bf9096a0d63bae555d211
fburman666 <fburman@gmail.com>
2025-01-18 09:41:01 +0100

    test

--- 1_rabatt.py
+++ 1_rabatt.py
@@ -1,1 +1,16 @@
-#kommentar
+#is_member = False
+level1 = 100
+level2 = 300
+#discount = 0
+
+price = input ("Välkommen, köp något dyrt: ")
+price = float(price)
+if level2 > price > level1:
+    print ("Grattis! Du har avancerat till nivå 1 och får 10% rabatt.")
+    discount = 10
+if price >= level2:
+    print ("Grattis! Du har avancerat till nivå 2 och får 25% rabatt.")
+    discount = 25
+
+final_price = price * (100 - discount) / 100
+print("efter rabatter blir priset...", final_price, "rabatt: ", discount)
